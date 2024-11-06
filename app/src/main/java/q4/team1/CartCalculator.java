4aa3c4b25b2903f5dcae062fee3869e244cd6133
marleomac3 <mlmcwilliams244@gmail.com>
2024-11-06 07:27:28 -0800

    feat: added exceptions for an empty cart and non existent item (#9)
    * feat: adds project structure
    
    * feat: removes App.java
    
    * feat: implements CartCalculator. methods on CartItems and on the ShoppingCart class do not exist yet
    
    * feat: adds NegativeDiscountException
    
    * feat: Created CartIsEmptyException and ItemNotFoundException
    
    ---------
    
    Co-authored-by: XavierCruz5106 <xrcruz244@gmail.com>
    Co-authored-by: Marion McWilliams <marionmcwilliams@Marions-MacBook-Air.local>

--- app/src/main/java/q4/team1/CartCalculator.java
+++ app/src/main/java/q4/team1/CartCalculator.java
@@ -1,5 +1,18 @@
 package q4.team1;
 
+
+import java.util.ArrayList;
+
 public class CartCalculator {
 
-}
+  public double calculatePrice(ShoppingCart cart){
+    ArrayList<CartItem> items = cart.getCartItems();
+    double sum = 0;
+
+    for (CartItem item: items) {
+      sum += item.getPrice(); // the price property could store the discounted price
+    }
+
+
+    return sum;
+  }
