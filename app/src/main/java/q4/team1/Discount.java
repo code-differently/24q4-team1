11cd84b99ab862fd54420ade297706c6aafddca9
Marion McWilliams <marionmcwilliams@Marions-MacBook-Air.local>
2024-11-06 11:51:58 -0500

    feat: inital commit of user story 4 work

--- app/src/main/java/q4/team1/Discount.java
+++ app/src/main/java/q4/team1/Discount.java
@@ -1,5 +1,15 @@
 package q4.team1;
 
-public abstract class Discount {
+import java.util.ArrayList;
 
+public abstract class Discount {
+    private double cartTotal;
+    private ArrayList<CartItem> items;
+
+    public Discount(double cartTotal, ArrayList<CartItem> items) {
+    this.cartTotal = cartTotal;
+    this.items = items;
+    }
+
+    public abstract double calculateDiscount();
 }
--- app/src/main/java/q4/team1/PercentDiscount.java
+++ app/src/main/java/q4/team1/PercentDiscount.java
@@ -1,5 +1,26 @@
 package q4.team1;
 
-public class PercentDiscount extends Discount {
+import java.util.ArrayList;
 
+public class PercentDiscount extends Discount {
+    private ArrayList<CartItem> items;
+    private double discountRate;
+
+    public PercentDiscount(double cartTotal, ArrayList<CartItem> items, double discountRate) {
+        super(cartTotal, items);
+        this.discountRate = discountRate;
+    }
+
+    public double getDiscountRate() {
+        return discountRate;
+    }
+
+    @Override
+    public double calculateDiscount() {
+        double sum = 0;
+        for (CartItem item: items) {
+            sum += item.getPrice();
+        }
+        return sum * 0.1;
+    }
 }
