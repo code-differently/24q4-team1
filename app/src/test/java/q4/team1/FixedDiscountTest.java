63214b22ed821ac76eab929629968a6f69496f9a
XavierCruz5106 <superguyman121@gmail.com>
2024-11-07 12:42:33 -0500

    feat: adds negative Test for FixedDiscount

--- app/src/test/java/q4/team1/FixedDiscountTest.java
+++ app/src/test/java/q4/team1/FixedDiscountTest.java
@@ -1,5 +1,20 @@
 package q4.team1;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import q4.team1.exceptions.NegativeDiscountException;
+
 public class FixedDiscountTest {
 
+  @Test
+  public void testDiscountException_AmountIsNegative(){
+
+    assertThrows(NegativeDiscountException.class, () -> {
+      new FixedDiscount(0, null, -99);
+    });
+
+  }
 }
