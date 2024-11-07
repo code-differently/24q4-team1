5d35d9bab15084926fd58e4ed295d408858b029f
Marion McWilliams <marionmcwilliams@Marions-MacBook-Air.local>
2024-11-07 16:23:11 -0500

    feat: Created test for Percent discount. Abstract test WIP

--- app/src/test/java/q4/team1/PercentDiscountTest.java
+++ app/src/test/java/q4/team1/PercentDiscountTest.java
@@ -1,5 +1,26 @@
 package q4.team1;
 
-public class PercentDiscountTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
+public class PercentDiscountTest {
+    ArrayList<CartItem> items;
+    @Test
+    public void testCalculateDiscount() {
+       // Arrange
+       items = new ArrayList<CartItem>();
+       CartItem item1 = new CartItem("Item1", 1, 20.0);
+       CartItem item2 = new CartItem("Item2", 1, 10.0);
+       items.add(item1);
+       items.add(item2);
+       PercentDiscount percentDiscount = new PercentDiscount(items, 0.5);
+
+       // Act
+       double discountAmount = percentDiscount.calculateDiscount();
+
+       // Assert
+        assertEquals(15.0, discountAmount);
+    }
 }
