7f9bfbae4e353738c8f05308cb7f618582ff971b
XavierCruz5106 <superguyman121@gmail.com>
2024-11-07 13:17:35 -0500

    refactor: removed unused imports

--- app/src/test/java/q4/team1/FixedDiscountTest.java
+++ app/src/test/java/q4/team1/FixedDiscountTest.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import q4.team1.exceptions.ItemNotFoundException;
