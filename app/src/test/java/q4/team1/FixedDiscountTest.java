203dc213710d849af011c222db6ea13f85d0bb0d
XavierCruz5106 <superguyman121@gmail.com>
2024-11-07 12:58:35 -0500

    feat: adds null test, discount works

--- app/src/test/java/q4/team1/FixedDiscountTest.java
+++ app/src/test/java/q4/team1/FixedDiscountTest.java
@@ -1,10 +1,14 @@
 package q4.team1;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import q4.team1.exceptions.ItemNotFoundException;
 import q4.team1.exceptions.NegativeDiscountException;
 
 public class FixedDiscountTest {
@@ -13,8 +17,25 @@ public class FixedDiscountTest {
   public void testDiscountException_AmountIsNegative(){
 
     assertThrows(NegativeDiscountException.class, () -> {
-      new FixedDiscount(0, null, -99);
+      new FixedDiscount(new ArrayList<CartItem>(), -99);
     });
+  }
 
+  @Test
+  public void testDiscountException_ItemsAreNull(){
+    assertThrows(ItemNotFoundException.class, () -> {
+      new FixedDiscount(null, 2);
+    });
+  }
+
+  @Test
+  public void testCalculateDiscountWorks(){
+    CartItem item = new CartItem("Banana", 2, 5);
+    ArrayList<CartItem> items = new ArrayList<>();
+    items.add(item);
+
+    Discount fixed = new FixedDiscount(items, 2);
+
+    assertEquals(8, fixed.calculateDiscount());
   }
 }
