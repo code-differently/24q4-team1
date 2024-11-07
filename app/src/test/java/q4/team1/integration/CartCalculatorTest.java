0b8d6a28bb6f9f16c7b3704b0e439eae4f3699c7
XavierCruz5106 <superguyman121@gmail.com>
2024-11-07 10:34:39 -0500

    feat: add cart

--- app/src/test/java/q4/team1/integration/CartCalculatorTest.java
+++ app/src/test/java/q4/team1/integration/CartCalculatorTest.java
@@ -4,16 +4,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import q4.team1.CartCalculator;
+import q4.team1.ShoppingCart;
 
 public class CartCalculatorTest {
   CartCalculator checkout;
+  ShoppingCart cart;
   @BeforeEach
   public void setUp(){
     checkout = new CartCalculator();
+    cart = new ShoppingCart();
   }
 
   @Test
   public void testCalculatePriceReturnsExpectedPrice(){
-    
+
   }
 }
