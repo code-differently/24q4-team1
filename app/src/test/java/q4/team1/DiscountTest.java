73acc08d64d868af432c4f787f13d9fe5d15cbdc
marleomac3 <mlmcwilliams244@gmail.com>
2024-11-08 09:57:54 -0800

    feat: Generated unit tests for PercentDiscount and Discount abstract classes (#25)
    * feat: Created test for Percent discount. Abstract test WIP
    
    * feat: Generated unit tests for Discount abstract class
    
    * feat: Added tests to abstract and PercentDiscount classes
    
    ---------
    
    Co-authored-by: Marion McWilliams <marionmcwilliams@Marions-MacBook-Air.local>
    Co-authored-by: Marion McWilliams <marionmcwilliams@mac.myfiosgateway.com>

--- app/src/test/java/q4/team1/DiscountTest.java
+++ app/src/test/java/q4/team1/DiscountTest.java
@@ -1,5 +1,54 @@
 package q4.team1;
 
-public class DiscountTest {
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import q4.team1.exceptions.ItemNotFoundException;
+import q4.team1.exceptions.NegativeDiscountException;
+
+public class DiscountTest {
+    private ArrayList<CartItem> items;
+
+    private static class TestDiscount extends Discount {
+        double discount;
+        public TestDiscount(ArrayList<CartItem> items, double discount) {
+            super(items);
+            this.discount = discount;
+        }
+
+        @Override
+        public double calculateDiscount() throws NegativeDiscountException {
+            if (discount < 0) {
+                throw new NegativeDiscountException("Error cannot have a negative discount");
+            }
+            return 10.0;
+        }
+    }
+
+    @BeforeEach
+    void setUp() {
+        items = new ArrayList<CartItem>();
+        items.add(new CartItem("Item1", 1, 20.0));
+    }
+
+    @Test
+    void testDiscount_NullItems() {
+        assertThrows(ItemNotFoundException.class, () -> {new TestDiscount(null, 10.0);});
+    }
+
+    @Test
+    void testDiscount_ValidItems() {
+        TestDiscount discount = new TestDiscount(items, 10.0);
+        Assertions.assertEquals(10.0, discount.calculateDiscount());
+    }
+
+    @Test
+    void testDiscount_NegativeDiscount() {
+        assertThrows(NegativeDiscountException.class, () -> {new TestDiscount(items, -10.0).calculateDiscount();});
+    }
 }
