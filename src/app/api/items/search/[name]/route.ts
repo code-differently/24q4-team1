ead0a5102eb15922f5a7bb26f8d2037c726f0c80
XavierCruz5106 <superguyman121@gmail.com>
2024-12-05 14:12:36 -0500

    feat: added swagger docs for api/items/search/name

--- src/app/api/items/search/[name]/route.ts
+++ src/app/api/items/search/[name]/route.ts
@@ -1,10 +1,155 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+/**
+ * @swagger
+ * /api/items/search/{name}:
+ *   get:
+ *     description: Search for products by name.
+ *     parameters:
+ *       - in: path
+ *         name: name
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The name of the product to search for.
+ *     responses:
+ *       200:
+ *         description: Successful product retrieval
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 product:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       example: 2
+ *                     title:
+ *                       type: string
+ *                       example: "Eyeshadow Palette with Mirror"
+ *                     description:
+ *                       type: string
+ *                       example: "The Eyeshadow Palette with Mirror offers a versatile range of eyeshadow shades for creating stunning eye looks. With a built-in mirror, it's convenient for on-the-go makeup application."
+ *                     category:
+ *                       type: string
+ *                       example: "beauty"
+ *                     price:
+ *                       type: number
+ *                       format: float
+ *                       example: 19.99
+ *                     discountPercentage:
+ *                       type: number
+ *                       format: float
+ *                       example: 5.5
+ *                     rating:
+ *                       type: number
+ *                       format: float
+ *                       example: 3.28
+ *                     stock:
+ *                       type: integer
+ *                       example: 44
+ *                     tags:
+ *                       type: array
+ *                       items:
+ *                         type: string
+ *                       example: ["beauty", "eyeshadow"]
+ *                     brand:
+ *                       type: string
+ *                       example: "Glamour Beauty"
+ *                     sku:
+ *                       type: string
+ *                       example: "MVCFH27F"
+ *                     weight:
+ *                       type: number
+ *                       example: 3
+ *                     dimensions:
+ *                       type: object
+ *                       properties:
+ *                         width:
+ *                           type: number
+ *                           example: 12.42
+ *                         height:
+ *                           type: number
+ *                           example: 8.63
+ *                         depth:
+ *                           type: number
+ *                           example: 29.13
+ *                     warrantyInformation:
+ *                       type: string
+ *                       example: "1 year warranty"
+ *                     shippingInformation:
+ *                       type: string
+ *                       example: "Ships in 2 weeks"
+ *                     availabilityStatus:
+ *                       type: string
+ *                       example: "In Stock"
+ *                     reviews:
+ *                       type: array
+ *                       items:
+ *                         type: object
+ *                         properties:
+ *                           rating:
+ *                             type: integer
+ *                             example: 4
+ *                           comment:
+ *                             type: string
+ *                             example: "Very satisfied!"
+ *                           date:
+ *                             type: string
+ *                             format: date-time
+ *                             example: "2024-05-23T08:56:21.618Z"
+ *                           reviewerName:
+ *                             type: string
+ *                             example: "Liam Garcia"
+ *                           reviewerEmail:
+ *                             type: string
+ *                             example: "liam.garcia@x.dummyjson.com"
+ *                     returnPolicy:
+ *                       type: string
+ *                       example: "30 days return policy"
+ *                     minimumOrderQuantity:
+ *                       type: integer
+ *                       example: 32
+ *                     meta:
+ *                       type: object
+ *                       properties:
+ *                         createdAt:
+ *                           type: string
+ *                           format: date-time
+ *                           example: "2024-05-23T08:56:21.618Z"
+ *                         updatedAt:
+ *                           type: string
+ *                           format: date-time
+ *                           example: "2024-05-23T08:56:21.618Z"
+ *                         barcode:
+ *                           type: string
+ *                           example: "2817839095220"
+ *                         qrCode:
+ *                           type: string
+ *                           example: "https://assets.dummyjson.com/public/qr-code.png"
+ *                     images:
+ *                       type: array
+ *                       items:
+ *                         type: string
+ *                       example:
+ *                         - "https://cdn.dummyjson.com/products/images/beauty/Eyeshadow%20Palette%20with%20Mirror/1.png"
+ *                     thumbnail:
+ *                       type: string
+ *                       example: "https://cdn.dummyjson.com/products/images/beauty/Eyeshadow%20Palette%20with%20Mirror/thumbnail.png"
+ *       404:
+ *         description: Item not found
+ *       500:
+ *         description: Server error
+ */
+
 export async function GET(
   request: NextRequest,
   { params }: { params: { name: string } }
 ) {
   const { name } = params;
+  console.log(encodeURIComponent(name))
 
   const apiUrl = `https://dummyjson.com/products/search?q=${encodeURIComponent(name)}`;
 
