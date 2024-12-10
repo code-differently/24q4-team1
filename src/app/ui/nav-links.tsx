e7f2ffa3b7306b4a9ca0e643fabc2892eeefe316
Marion McWilliams <marionmcwilliams@Marions-MacBook-Air.local>
2024-12-10 09:32:08 -0500

    feat: modigied the nav bar alignment and modified sign in text

--- src/app/ui/nav-links.tsx
+++ src/app/ui/nav-links.tsx
@@ -9,27 +9,28 @@ import {
   } from "@clerk/nextjs";
 export default function NavLinks() {
     return (
-        <div className="flex-grow content-center absolute z-10 translate-y-[7px] lg:translate-x-[1590px]">
+        <div className="flex-grow content-center absolute z-10 translate-y-[7px] lg:translate-x-[1530px]">
             <ul className="flex justify-end gap-4 font-medium text-white">
-            <li>
-                <Link className="rounded-full border border-solid border-transparent dark:hover:text-teal-800"
-                 href="/cart">View Cart</Link>
-            </li>
-            <li>
-                <Link className="rounded-full border border-solid border-transparent dark:hover:text-teal-800"
-                 href="/products">Purchase History</Link>
-            </li>
-            <li>
-            <ClerkProvider>
-              <SignedOut>
-                <SignInButton className="pr-[7px] rounded-full border border-solid border-transparent dark:hover:text-teal-800"/>
-              </SignedOut>
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
-        </ClerkProvider>
-            </li>
-        </ul>
+                <li>
+                    <ClerkProvider>
+                        <SignedOut>
+                            <SignInButton className="rounded-full border border-solid border-transparent dark:hover:text-teal-800">Log In/Register</SignInButton>
+                            
+                        </SignedOut>
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn>
+                    </ClerkProvider>
+                </li>
+                <li>
+                    <Link className="rounded-full border border-solid border-transparent dark:hover:text-teal-800"
+                    href="/cart">View Cart</Link>
+                </li>
+                <li>
+                    <Link className="rounded-full border border-solid border-transparent dark:hover:text-teal-800"
+                    href="/products">Purchase History</Link>
+                </li>
+            </ul>
         </div>
     );
 }
